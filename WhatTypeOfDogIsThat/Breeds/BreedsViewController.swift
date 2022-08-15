2d13a4c1ebc6035988d3126b93ed02486aa4dfdb
Paul Ramsay <pramsay@nn4m.com>
2022-08-15 16:01:52 +0100

    Add-App-Coordinator
    The app coordinator will handle the basic app wide navigation, If we need to add a basic tab bar we can here.

---
+++ WhatTypeOfDogIsThat/Breeds/BreedsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BreedsViewController.swift
+//  WhatTypeOfDogIsThat
+//
+//  Created by Paul Ramsay on 15/08/2022.
+//
+
+import UIKit
+
+protocol BreedsViewPresenting: AnyObject {}
+
+class BreedsViewController: UIViewController {
+
+    var presenter: BreedsViewPresenting!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TODO: begin Breeds Implementation
+    }
+}
